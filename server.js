ecf414e27d80185437b42c39725ddd5ad5cdaaef
DariaKat <51202251+DariaKat@users.noreply.github.com>
2020-08-04 12:46:58 +0300

    update

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const cors = require('cors');
 
 //app.use(cors());
 
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   res.header('Access-Control-Allow-Credentials', true);
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header(
